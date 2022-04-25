ae0658d9c1c7c2470d7c4f73234d88644dd6f20f
Zach Davis <zacharydavis92@gmail.com>
2022-04-25 16:46:14 -0500

    'added file-loaders'

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,52 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
     },
-    // Use plugins to define variables to use the unstalled npm package. When using libraries that depend on the use of global variables,
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    // Use plugins to define variables to use the installed npm package. When using libraries that depend on the use of global variables,
     // you must tell webpack to make exceptions for them using webpack.ProvidePlugin
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
